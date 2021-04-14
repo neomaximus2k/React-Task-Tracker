5c3219af7631fe244ffc8fb2b51f3c70ee8ef45e
Matthew Bagley <matthew.bagley@paramiliar.com>
2021-04-14 09:44:47 +0100

    Stage2

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # React-Task-Tracker
+
+Example react task tracker.  The tracker is split into 3 seperate commits:
+
+1. Simple system with static data. **init**
+2. Second stage is with a JSON server and routing. **Stage2**
+3. Final stage is with a Laravel backend. **TBD**
+
+## JSON Server
+JSON server is installed using https://github.com/typicode/json-server
+```
+npm install json-server
+```
+Add the following to your `package.json` file in the scripts area
+```
+"server": "json-server --watch db.json --port 5000"
+```
+you can then run the JSON server using `npm run server`
+
+## Routing
+Routing is handled by react routing dom
+```
+npm install react-router-dom
+```
--- src/App.js
+++ src/App.js
@@ -1,63 +1,104 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
 
 function App() {
 
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Task 1',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Task 2',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Task 3',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-])
+
+    getTasks()
+  }, [])
 
 // Functions declared here
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask])
+/** Get the tasks from the JSON server */
+const fetchTasks = async() => {
+  const resp = await fetch('http://localhost:5000/tasks')
+  const data = await resp.json()
+  return data
+}
+
+const fetchTask = async ( id) => {
+  const resp = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await resp.json()
+  return data
+}
+
+const addTask = async (task) => {
+  const resp = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+  const data = await resp.json()
+  setTasks([...tasks, data])
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = {id, ...task}
+  // setTasks([...tasks, newTask])
 }
 /** Delete Task - deletes the selected task*/ 
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE'
+  })
   setTasks(tasks.filter((task) => task.id !== id ))
 }
 
 /**
  * Toggles the reminder for the task
  */
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+  const resp = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method:'PUT',
+    headers:{
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updateTask)
+  })
+
+  const data = await resp.json()
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task))
 }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? 
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
-          : 
-        'No tasks'
-      }
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? 
+              <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
+                : 
+              'No tasks'
+            }
+          </>
+        )
+        } />
+        <Route path='/about' component={About} />
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
